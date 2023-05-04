5d15df890b233263c9cdf84676badb2d94f8836c
gwynbleiddrivia <yasinzme2@gmail.com>
2023-05-04 06:09:07 +0600

    cors configured, chef data is now showing on server

--- afghan-cuisine-corner-server/index.js
+++ afghan-cuisine-corner-server/index.js
@@ -1,10 +1,21 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
+const cors = require('cors')
+
+app.use(cors())
+
+const chefs = require('./data/chefs.json')
+
 
 app.get('/',(req,res)=>{
 	res.send("Afghan Cuisine Corner server is running")
 })
+app.get('/chefs',(req,res)=>{
+	res.send(chefs)
+})
+
+
 
 app.listen(port,()=>{
 	console.log('Afghan Cuisine API is running on port:${port}')
